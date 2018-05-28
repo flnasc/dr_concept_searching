08e7d6c8a59209438772aeb3b2b345887568b257
Dylan Hayton-Ruffner <DylanHR@dhcp-218-248.wireless.bowdoin.edu>
2018-05-28 12:34:26 -0400

    Documented cofih_test.py

--- code/cofih_test.py
+++ code/cofih_test.py
@@ -1,3 +1,12 @@
+"""
+	Author: Dylan Hayton-Ruffner
+	Description: Test the CoFiH class provided by Louis Chartrand on Documents from Digital Ricoeur 
+	Status: Imports set up and skeleton started. 
+	Todo: Build input Document-Term matrix from data files
+
+"""
+
+
 import cofih as c
 import numpy as np
 import scipy.sparse as sp
@@ -8,6 +17,9 @@ from itertools import islice
 from lazysorted import LazySorted
 from scipy.spatial.distance import cdist
 import brisk
+
+
+#************************** Skeleton **************************#
 
 test_matrix = sp.csr.csr_matrix((90,100), dtype=int)
 cofih = c.CoFiH(test_matrix)
