fb85f14ea0f425adb257a80c27b7e2cad029e9e1
Dylan Hayton-Ruffner <DylanHR@dhcp-221-175.wireless.bowdoin.edu>
2018-05-31 17:22:06 -0400

    Matrix with term frequencies

--- code/xml_seg.py
+++ code/xml_seg.py
@@ -4,14 +4,18 @@
    Status: Implemented paragraph extraction and vectorization -> WIP
    ToDo: 
        1.) work on cos and euclidean dist calcs
+       2.) Format Maticies as percent frequencies
    NOTES:
    	   1.) can I use unicode strings with CountVect? -> looks like yes
    	   2.) BS4 xml parser only works if TEI is present
+   	   3.) IMPORTANT!!! CURRENTLY CALCULATING PARA SIZE BEFORE TEXT PROCESSING
 """
 
 import bs4 as Soup
 import nltk
 from sklearn.feature_extraction.text import CountVectorizer
+import re
+
 
 def main():
 
@@ -24,21 +28,43 @@ def main():
 	
 	#get contents of each paragraph tag and add them to a list type 'corpus'
 	corpus = []
+	corpusWCS = []
 	for i in range(0, len(doc_para)):
 		print(doc_para[i].get_text())
+		numbW = re.findall(r'\b\w+', doc_para[i].get_text())
 		corpus.append(doc_para[i].get_text())
+
+
 
 	print("----CORPUS----")
   	print(corpus)
+  
 
   	#Use CountVectorizer to get sparse matrix with
 	vectorizer = CountVectorizer(stop_words = 'english', lowercase= True)
+	
 	vectorizer.fit_transform(corpus)
 	cmatrix = vectorizer.transform(corpus)
-	print("----CountVectorizer INFO----")
+	cmatrix.asfptype()
+	cmatDense = cmatrix.todense().astype(float)
+	rows, cols = cmatrix.get_shape()
+	
+	
+	print("----CountVectorizer INFO BEFORE SIZE ADJ----")
+	print(cmatDense)
 	print(vectorizer.get_feature_names())
 	print(type(vectorizer.vocabulary))
-	print(cmatrix.todense())
+	
+	#adjust cmatrix for the size of each paragraph (words  now constitute precentages)
+	for i in range(0, rows):
+		for j in range(0, cols):
+			numOcc = cmatDense.item(i,j)
+			totalWords = cmatrix.getrow(i).sum()
+			cmatDense.itemset((i,j), numOcc/totalWords)
+	print(cmatDense)
+
+
+
 
 
 
