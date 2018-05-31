7015a93cf12e3bc85af8f551a3fd2ea2042a5436
Dylan Hayton-Ruffner <DylanHR@Dylans-MBP.lan>
2018-05-31 11:46:41 -0400

    Implemented paragraph extraction and vectorization; TODO: distance calcs, added test do

--- code/xml_seg.py
+++ code/xml_seg.py
@@ -1,9 +1,12 @@
 """ 
    Author: Dylan Hayton-Ruffner
    Description: Parses am XML document, segmenting it into paragraphs
-   Status: Inital Skeletion -> WIP
+   Status: Implemented paragraph extraction and vectorization -> WIP
    ToDo: 
-       1.) fix NONETYPE ERROR WITH cv.fit_transform
+       1.) work on cos and euclidean dist calcs
+   NOTES:
+   	   1.) can I use unicode strings with CountVect? -> looks like yes
+   	   2.) BS4 xml parser only works if TEI is present
 """
 
 import bs4 as Soup
@@ -11,18 +14,31 @@ import nltk
 from sklearn.feature_extraction.text import CountVectorizer
 
 def main():
+
+	#get text data from file as a raw string, parse with bs4 and extract paragraph tags -> list of bs4.element.Tag objects
 	filepath = raw_input("Filepath to desired document: ")
 	print("DESIRED FILE: " + filepath)
 	doc_string = load_document(filepath)
 	doc_soup = Soup.BeautifulSoup(doc_string, 'xml') 
 	doc_para = doc_soup.find_all('p') #use beautiful soup to find all contents fo the para 
+	
+	#get contents of each paragraph tag and add them to a list type 'corpus'
+	corpus = []
+	for i in range(0, len(doc_para)):
+		print(doc_para[i].get_text())
+		corpus.append(doc_para[i].get_text())
 
-	print(doc_para)
-	corpus=  ["Knowing yourself is the beginning of all wisdom",
-    "What is a friend? A friend is single soul dwelling in two bodies"]
+	print("----CORPUS----")
+  	print(corpus)
+
+  	#Use CountVectorizer to get sparse matrix with
 	vectorizer = CountVectorizer(stop_words = 'english', lowercase= True)
-	vectorizer.fit_transform(doc_para)
+	vectorizer.fit_transform(corpus)
+	cmatrix = vectorizer.transform(corpus)
+	print("----CountVectorizer INFO----")
 	print(vectorizer.get_feature_names())
+	print(type(vectorizer.vocabulary))
+	print(cmatrix.todense())
 
 
 
