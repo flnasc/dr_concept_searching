6a39d261a08f95bb873af87a1e39e53bdcc1b654
Dylan Hayton-Ruffner <DylanHR@Dylans-MacBook-Pro.local>
2018-08-08 09:56:18 -0400

    documented

--- code/ML/ML-Approach.py
+++ code/ML/ML-Approach.py
@@ -1,27 +1,34 @@
 """ 
    Author: Dylan Hayton-Ruffner
-   Description: Takes a document in xml format and splits it into paragraphs. Returns the set of paragraphs that contain sentances whose subject
-   is a query read-in from the user. 
+   Description: This program utilizes NLP and machine learning to extract concepts from philosophical texts. The user
+   inputs a the filepath of an xml file containing a text. The program splits that text into paragraphs and then (via NLP and ML)
+   trains a model to search for each of the concepts list in the text document at  CONCEPT_PATH. For each concept with a significant presence 
+   in the document, the program writes an Excel file containing the parameterization of the query, the segments, and basic cell programming for 
+   segment scoring.
 
-   
-   Status: WIP
-   ToDo: 
-      
-   NOTES:
-   	   
-   	   
 """
+#percentage of the training data used for the test set
 TEST_SET_SIZE = 0.2
+
+#the minimum word count of a paragraph, those below are discared
 MIN_WORD_COUNT = 10
+
+#the minimum number of segments that must be returned from the NLP query
 MIN_SEG_COUNT = 10
+
+#the threshold for the sgd_clf's decision function, increasing reduces the number of segs returned
 DEC_FUNC_THRESHOLD = 200
+
+#vectorizer paramerters
 MAX_DF = 0.95
 MIN_DF = 2
-TOP_N_SEGS=10
-MIN_DF = 0.01
-MAX_DF = 0.95
+
+
 FILETYPE = 'xml'
+
+#path to the conepts file
 CONCEPTS_PATH = "../../data/concepts.txt"
+
 import bs4 as Soup
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.decomposition import LatentDirichletAllocation
@@ -49,34 +56,38 @@ class LemmaTokenizer(object):
      def __call__(self, doc):
          return [self.wnl.lemmatize(t) for t in re.findall(u'(?u)\\b\\w\\w\\w+\\b', doc)]
 
-
-
 ############################# MAIN #############################
-
 def main():
-	print("\n-----LDA CONCEPT DETECITON-----")
-	text_corpus, raw_corpus = load_corpus('v')
-	query = ["symbol", "symbols", "symbolism", 'symbolisms'] #concept
+	print("\n-----ML CONCEPT DETECITON-----")
 
-	#get the training data
+	#load and evaluate the text
+	text_corpus, raw_corpus = load_corpus('v')
+	num_segs = len(text_corpus)
+
 	concepts_raw = load_document(CONCEPTS_PATH)
 	concepts = parse_concepts(concepts_raw)
-	num_segs = len(text_corpus)
+	
+	#get training data
 	nlp = spacy.load('en_core_web_sm')
-	positives = [] #the segments that contain the query in subject form
-	pos_ids = [] #holds segement ids (the index of the segment in the text_corpus list)
-	num_w_query = 0
 
-	#find positive segments (instances of concept)
+	#the segments that contain the query in subject form
+	positives = [] 
+
+	#holds segement ids (the index of the segment in the text_corpus list)
+	pos_ids = [] 
 
 
+	#a set of arrays where each array holds the information (concet, postives, ids) for the coresponding concept
 	concept_queries = []
-	short_concepts = []
+	#the set of all positives, i.e. the entrie vocabulary
 	full_set = []
 
+	#find positive segments (instances of concept) for each concept in the provided file, add to concept_queries
 	for i in range(0,len(concepts)):
 
 		start = time.time()
+
+		#use nlp to seach the corpus and return all segments with the query as a subject, also returns the segment ids (index)
 		positives, pos_ids = search_corpus(text_corpus, concepts[i], nlp)
 		if len(positives) > MIN_SEG_COUNT:
 			concept_queries.append([concepts[i], positives, pos_ids])
@@ -87,14 +98,16 @@ def main():
 
 	
 
-
+	#for each concept, traing the model and use it to find segments within the corpus
 	for i in range(0,len(concept_queries)):
 
 		positives = concept_queries[i][1].copy()
 		pos_ids = concept_queries[i][2].copy()
+
 		negatives = []
 		neg_ids = []
 
+		#go through the other concepts and add to the negatives set all segments no present in the positives (locate true negatives)
 		for j in range(0, len(concept_queries)):
 
 			posb_neg = concept_queries[j][1].copy()
@@ -102,72 +115,40 @@ def main():
 			for k in range(0, len(posb_neg_ids)):
 				if (posb_neg_ids[k] not in pos_ids) and (posb_neg_ids[k] not in neg_ids) :
 					negatives.append(posb_neg[k])
-					#print(posb_neg[k])
 					neg_ids.append(posb_neg_ids[k])
 
-		
-		#print("Negatives, before",negatives)
+	
 		training, test, train_target, test_target = build_train_test_sets(positives, negatives, TEST_SET_SIZE)
-
-		
-		
-
-
-
 
 
 		vectorizer = CountVectorizer(stop_words = 'english',lowercase= True, max_df=MAX_DF, min_df=MIN_DF, tokenizer=LemmaTokenizer())
-		vectorizer.fit(full_set)	
+		vectorizer.fit(text_corpus)	#NOTE: used to use full_set instead -> does this make a difference 
 		training_matrix = vectorizer.transform(training).todense()
 		corpus_matrix = vectorizer.transform(text_corpus).todense()
 		test_matrix =  vectorizer.transform(test)
 		sgd_clf = SGDClassifier(random_state=42)
-		#print(concept_queries[i][0],training, train_target)
 		sgd_clf.fit(training_matrix, train_target)
 
 		results = []
 		res_ids = []
 		
+		#get scores for entire corpus and, if above threshold, add to corpus
 		scores = sgd_clf.decision_function(corpus_matrix)
 		for k in range(0, len(scores)):
 			if scores[k] > DEC_FUNC_THRESHOLD:
 				results.append(text_corpus[k])
 
-		print(concept_queries[i][0],len(training), len(test), len(results))
+		print(concept_queries[i][0], len(training), len(test), len(results))
 
+		#each array now contains the results of the concept_query
 		concept_queries[i].append(results)
-		
-
 		write_output_file_xlsx(concept_queries[i], "../../data/symbolism-of-evil.xml", len(text_corpus), MIN_WORD_COUNT,DEC_FUNC_THRESHOLD)
 
 
 
-
-	# write_output_file(results)
-
-
-	# print(len(negatives), "negatives")
-	# print(len(positives), "positives")
-	# print(len(results), "results")
-	# print(len(training), "training len")
-	# print(len(test),"testing len")
-	# print(recall_score(test_target, train_pred), "recall")
-	# print(precision_score(test_target, train_pred), "prescision")
-	# print(f1_score(test_target, train_pred), "f1")
-	# print(roc_auc_score(test_target, train_pred), "roc_auc_score")
-
-	# target_scores = cross_val_predict(sgd_clf, training_matrix, train_target, cv=3, method="decision_function")
-	# precisions, recalls,thresholds = precision_recall_curve(train_target, target_scores)
-	# pre_rec_plot(precisions, recalls, thresholds)
-	# plt.show()
-	
-
 	return 0
 
-
-
 ############################# LOAD DATA ############################# 
-
 def load_document(filepath):
 	"""	
 		Description:Opens and loads the file specified by filepath as a raw txt string; assumes valid text file format.
@@ -182,13 +163,18 @@ def load_document(filepath):
 	file.close()
 	
 	return file_string
-
 def parse_concepts(concepts_raw):
+	"""	
+		Description:Takes the raw string from the concepts file and parses it into a list of concepts.
+		Each line of the file is returned as a list of words.
+		Input: String -> filepath of file from current directory
+		Output: List where each index is a list of words corresponding to a line of the file.
+
+	"""
 	concepts = concepts_raw.split('\n')
 	for i in range(0, len(concepts)):
 		concepts[i] = concepts[i].split()
 	return concepts
-
 def load_corpus(v):
 	"""html and xml are supported"""
 
@@ -222,7 +208,6 @@ def load_corpus(v):
 			continue
 
 	return cleaned_corpus, raw_corpus
-
 def load_corpus_txt(c_size):
 	#works with a raw text file
 	filepath = input("Filepath to corpus: ")
@@ -243,7 +228,6 @@ def load_corpus_txt(c_size):
 	return final
 
 ############################# Results Format ############################# 
-
 def write_output_file(seg_list):
 
 	
@@ -253,9 +237,6 @@ def write_output_file(seg_list):
 
 	for i in range(0, len(seg_list)):
 		new_f.write("\nSeg:" +str(seg_list[i])+ "\n")
-
-	
-
 def write_output_file_xlsx(concept_info, filepath, num_segs, min_seg_len, dec_func_threshold):
 	
 
@@ -318,12 +299,18 @@ def write_output_file_xlsx(concept_info, filepath, num_segs, min_seg_len, dec_fu
 	
 	workbook.close()
 
-#
 ############################# NLP ############################# 
-
 def contains_query(para,query, nlp):
-	"""Returns a bool representing whether the given paragraph (para -> list of sents) contains a sentance with the query (as subject)"""
+	"""	
+		Description:Returns a bool representing whether the given paragraph (para -> list of sents) contains a sentance with the query (as subject).
+		Input: para (list of strings) -> a list of the sentences in a paragraph from the text
+			   query (list) ->  list of words in the concept query
+			   nlp -> the nlp processing information ("en_core_web_sm") for spacy
+		Output: Bool
 
+	"""
+
+	#go through each sentence and see if sentence contains a word from the query
 	for i in range(0, len(para)):
 		words = word_tokenize(para[i])
 		words = [word.lower() for word in words]
@@ -337,6 +324,7 @@ def contains_query(para,query, nlp):
 		if not word_present:
 			continue
 
+		#if the query-word is present, check to see if it is the subject
 		doc = nlp(para[i])
 		for chunk in doc.noun_chunks:
 			
@@ -347,8 +335,15 @@ def contains_query(para,query, nlp):
 
 	return False
 def search_corpus(text_corpus, query, nlp):
-	"""Searchs the given list of text segments for intances in which a segment contains the query in subject form.
-	Returns all instances in a list. Returns a second list of the segment ids (their original index in text_corpus)"""
+	"""	
+		Description:Searchess the given list of text segments (paragraphs) for intances in which a segment contains the query in subject form..
+		Input: text_corpus (list of strings) -> a list of the paragraph in the text
+			   query (list) ->  list of words in the concept query
+			   nlp -> the nlp processing information ("en_core_web_sm") for spacy
+		Output: Bool
+
+	"""
+
 	tagged = []
 	tagged_ids = []
 	for i in range(0, len(text_corpus)):
@@ -363,48 +358,51 @@ def search_corpus(text_corpus, query, nlp):
 	return tagged, tagged_ids
 
 ############################# ML ############################# 	
-
 def build_train_test_sets(positives, negatives, test_set_size):
-	"""Takes two lists of true positive and true negative instances of the concept and combine them into training and test
-	sets. Returns these two sets. Also returns target arrays, for each of the sets. These arrays contain True wherever the 
-	corresponding indice of the set has a true positiv value."""
+	"""	
+		Description:Builds the training and test sets from the set of true negatives and true positives.
+		Input: positives (list) -> list of the paragraphs containing the queried concept.
+			   negatives (list) -> list of the paragraphs not containing the queried concept.
+			   test_set_size (float) -> percentage of the data to be used in test set
+		Output: the training and test sets, along with their target lists which contain "True" at each 
+		index with a positive instance of the concept
 
-	
-	#print("negatives after",negatives)
+	"""
+
+	#mark positives
 	for i in range(0, len(positives)):
 		positives[i] =  (True, positives[i])
 
-
-
-
-	
+	#divide data
 	train_pos, test_pos = positives[int(len(positives)*test_set_size):], positives[:int(len(positives)*test_set_size)]
 	train_neg, test_neg = negatives[int(len(negatives)*test_set_size):], negatives[:int(len(negatives)*test_set_size)]
 
+	#combine pos and neg data
 	train_pos.extend(train_neg)
 	test_pos.extend(test_neg)
 
+	#randomize order
 	np.random.shuffle(train_pos)
 	np.random.shuffle(test_pos)
 
+	#write train_target
 	train_target = train_pos.copy()
 	for i in range(0, len(train_pos)):
 		
-		#print(train_pos[i],type(train_pos[i]) is tuple)
 		train_target[i] = type(train_pos[i]) is tuple
 		if type(train_pos[i]) is tuple:
 			train_pos[i] = train_pos[i][1]
 
+	#write test_target
 	test_target = test_pos.copy()
 	for i in range(0, len(test_pos)):
 		test_target[i] = type(test_pos[i]) is tuple
 		if type(test_pos[i]) is tuple:
 			test_pos[i] =	test_pos[i][1]
 
+	#rename lists
 	training = train_pos
 	test = test_pos
-
-	
 
 	return training, test, train_target, test_target
 
