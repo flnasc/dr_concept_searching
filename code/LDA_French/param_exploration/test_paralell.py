5dc65fa47954e0e94347e4c6db240fd13af9abaa
DylanHR <hrdylan@gmail.com>
2019-10-06 21:41:55 -0400

    adds command line control to test_parallel

--- code/LDA_French/param_exploration/test_paralell.py
+++ code/LDA_French/param_exploration/test_paralell.py
@@ -1,16 +1,17 @@
 import time
 from multiprocessing import Pool
 import sys
-def test(num_workers=5):
+def test(size, num_workers=5):
     print('Running models...')
+    print(f'Size {size}, Workers {num_workers}')
     pool = Pool(num_workers)
     s = time.time()
-    res = pool.map(run_model, [0]*1000000)
+    res = pool.map(run_model, [(1,1)]*size)
     e = time.time()
     print(f'Entire job took {e - s} seconds')
 
-def run_model(set):
-    return 1
+def run_model(nums):
+    return nums[0] + nums[1]
 
 if __name__ == '__main__':
-    test(int(sys.argv[1]))
+    test(int(sys.argv[1]), num_workers=int(sys.argv[2]))
